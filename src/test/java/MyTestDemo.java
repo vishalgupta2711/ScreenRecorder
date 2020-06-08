ce9586eb51a754dc26c09327b38858a0ce484d72
Vishal Gupta <vishalgupta.2711@gmail.com>
2020-06-08 17:34:52 +0530

    code for recording screen

---
+++ src/test/java/MyTestDemo.java
@@ -0,0 +1,71 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/*
+ * 
+ * Author :  Vishal Gupta
+ * 
+ */
+
+public class MyTestDemo {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup(){
+		System.setProperty("webdriver.chrome.driver", "F:\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void navigationTest() throws Exception{
+		MyScreenRecorder.startRecording("navigationTest");
+		
+		driver.get("http://www.google.com");
+		driver.navigate().to("http://www.facebook.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		
+		MyScreenRecorder.stopRecording();
+	}
+	
+	@Test
+	public void navigation_FeatureTest() throws Exception{
+		MyScreenRecorder.startRecording("navigation_FeatureTest");
+		
+		driver.get("http://www.amazon.com");
+		driver.navigate().to("http://www.rediff.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().back();
+		driver.navigate().forward();
+		
+		MyScreenRecorder.stopRecording();
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+
+}
